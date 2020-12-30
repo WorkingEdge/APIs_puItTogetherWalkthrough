d2f3454015f96e7800f3d5301158713600dc73ec
WorkingEdge <jclry76@gmail.com>
2020-12-30 19:57:26 +0000

    Add repo response

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,7 +1,7 @@
 function userInformationHTML(user){
     return `
     <h2>${user.name}
-    <span class="small-name">(@<a href="${user.html_url}" target="_blank")>${user.login}</a> 
+    <span class="small-name">(@<a href="${user.html_url}" target="_blank">${user.login}</a>) 
     </span></h2>
     <div class="gh-content">
     <div class="gh-avatar">
@@ -11,6 +11,19 @@ function userInformationHTML(user){
     <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
     </div>`;
 }
+function repoInformationHTML(repos){
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list>No repos!</div>`;
+    }
+     let listItemsHTML = repos.map(function(repo){
+         return`<li>
+         <a href = "${repo.html_url}" target="_blank">${repo.name}</a></li>`
+     });
+     return `<div class="clearfix repo-list">
+     <p><strong>Repo List:</strong></p>
+     <ul>
+        ${listItemsHTML.join("\n")}</ul></div>`;
+
 
 function fetchGitHubInformation(event) {
     let username = $("#gh-username").val();
@@ -24,11 +37,14 @@ function fetchGitHubInformation(event) {
     <img src="assets/css/loader.gif" alt = "loading ..."/></div>`
     );
     $.when(
-        $.getJSON("https://api.github.com/users/${username}")
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function (response) {
-            let userData = response;
+        function (firstResponse, secondResponse) {
+            let userData = firstResponse[0];
+            let repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function (errorResponse) {
             if (errorResponse.status === 404) {
